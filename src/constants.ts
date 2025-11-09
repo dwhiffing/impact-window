cf5e7e6ef26382ea0704245b334077654cb471a8
Daniel Whiffing <danielwhiffing@gmail.com>
2025-11-09 15:05:34 -0500

    Reorganize constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,5 @@
 import * as Phaser from 'phaser'
 import { EnemyType, EnemyStats } from './types'
-export const PARTICLE_CONFIG = {
-  lifespan: { min: 100, max: 1200 },
-  scale: { min: 0.2, max: 1.5 },
-  speed: { min: 50, max: 100 },
-  alpha: { start: 1, end: 0 },
-  rotate: { start: 0, end: 1200 },
-  quantity: 0,
-}
 
 export const PLAYER_COLOR = 0x00ffcc
 export const PLAYER_CRUSH_COLOR = 0xffff00
@@ -25,15 +17,19 @@ export const DEAD_ZONE_SIZE = 3
 export const NUDGE_ZONE_SIZE = 40
 export const MAX_THUMB_SIZE = 100
 
-export const ENEMY_SIZE = 10
 export const ENEMY_COLOR = 0xff4444
 export const MULTI_SPEED_BOOST = 30
+
+export const MAX_ENERGY = 100
+export const ENERGY_RECHARGE_RATE = 12
+export const WEAK_LAUNCH_COST = 12
+export const FULL_LAUNCH_COST = 40
 
 export const ENEMY_STATS: Record<EnemyType, EnemyStats> = {
   grunt: {
     color: 0xff4444,
     speed: 60,
-    size: ENEMY_SIZE,
+    size: 10,
     health: 1,
     energyOnKill: 3,
     speedBoost: 120,
@@ -41,7 +37,7 @@ export const ENEMY_STATS: Record<EnemyType, EnemyStats> = {
   heavy: {
     color: 0x8888ff,
     speed: 35,
-    size: ENEMY_SIZE * 1.5,
+    size: 15,
     health: 2,
     energyOnKill: 6,
     speedBoost: 200,
@@ -59,7 +55,12 @@ export const TRAIL_CONFIG: Phaser.Types.GameObjects.Particles.ParticleEmitterCon
     active: false,
   }
 
-export const MAX_ENERGY = 100
-export const ENERGY_RECHARGE_RATE = 12
-export const WEAK_LAUNCH_COST = 12
-export const FULL_LAUNCH_COST = 40
+export const PARTICLE_CONFIG: Phaser.Types.GameObjects.Particles.ParticleEmitterConfig =
+  {
+    lifespan: { min: 100, max: 1200 },
+    scale: { min: 0.2, max: 1.5 },
+    speed: { min: 50, max: 100 },
+    alpha: { start: 1, end: 0 },
+    rotate: { start: 0, end: 1200 },
+    quantity: 0,
+  }
