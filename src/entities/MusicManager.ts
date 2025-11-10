51957ce245c72f76d36fd4d444202ba979c530aa
Daniel Whiffing <danielwhiffing@gmail.com>
2025-11-10 14:20:40 -0500

    Fixes music fade

--- src/entities/MusicManager.ts
+++ src/entities/MusicManager.ts
@@ -1,5 +1,7 @@
 const STORAGE_KEY = 'impact-window-audio-mode'
 
+const TARGET_MUSIC_VOLUME = 0.3
+const FADE_TIME = 250
 export class MusicManager {
   scene: Phaser.Scene
   currentMusic: Phaser.Sound.BaseSound | null = null
@@ -19,6 +21,14 @@ export class MusicManager {
       this.check()
     })
 
+    scene.input.keyboard!.off('keydown-N')
+    scene.input.keyboard!.on('keydown-N', () => {
+      // @ts-ignore
+      if (window.currentMusic?.volume < TARGET_MUSIC_VOLUME) return
+      this.trackIndex = (this.trackIndex + 1) % this.musicKeys.length
+      this.playNext(true)
+    })
+
     // @ts-ignore
     this.currentMusic = window.currentMusic
     if (this.currentMusic) return
@@ -27,31 +37,45 @@ export class MusicManager {
   }
 
   check() {
-    console.log('Music mode:', this.mode)
-    this.scene.sound.volume = this.mode !== 2 ? 0.3 : 0
+    this.scene.sound.volume = this.mode !== 2 ? TARGET_MUSIC_VOLUME : 0
     if (this.currentMusic)
-      (this.currentMusic as any).volume = this.mode === 0 ? 0.3 : 0
+      (this.currentMusic as any).volume =
+        this.mode === 0 ? TARGET_MUSIC_VOLUME : 0
   }
 
   playNext = (forcePlay = false) => {
     if (this.currentMusic && !forcePlay) return
-    const key = this.musicKeys[this.trackIndex]
-    this.currentMusic = this.scene.sound.add(key, { volume: 0 })
-
-    if (this.mode === 0) {
-      this.scene.tweens.add({
-        targets: this.currentMusic,
-        volume: 0.3,
-        duration: 250,
-      })
-    }
+    let target = { val: TARGET_MUSIC_VOLUME }
     // @ts-ignore
-    window.currentMusic = this.currentMusic
+    const updateMusic: Phaser.Types.Tweens.TweenOnUpdateCallback = ( _, __, ___, v, ) => this.currentMusic?.setVolume(v) // prettier-ignore
 
-    this.currentMusic.play()
-    this.currentMusic.once('complete', () => {
-      this.trackIndex = (this.trackIndex + 1) % this.musicKeys.length
-      this.playNext(true)
+    this.scene.tweens.add({
+      targets: target,
+      val: 0,
+      onUpdate: updateMusic,
+      duration: FADE_TIME,
+      onComplete: () => {
+        const key = this.musicKeys[this.trackIndex]
+        this.currentMusic = this.scene.sound.add(key, { volume: 0 })
+        this.currentMusic.play()
+
+        if (this.mode === 0) {
+          target = { val: 0 }
+          this.scene.tweens.add({
+            targets: target,
+            val: TARGET_MUSIC_VOLUME,
+            duration: FADE_TIME,
+            onUpdate: updateMusic,
+          })
+        }
+        // @ts-ignore
+        window.currentMusic = this.currentMusic
+
+        this.currentMusic.once('complete', () => {
+          this.trackIndex = (this.trackIndex + 1) % this.musicKeys.length
+          this.playNext(true)
+        })
+      },
     })
   }
 }
